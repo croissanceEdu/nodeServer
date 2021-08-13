de8e880a7ea65b7bc0bda113dcd54344ad91e351
JohnySR <srjvlr@outlook.com>
2021-08-13 20:09:33 +0530

    After Soccket 2

--- db.js
+++ db.js
@@ -3,8 +3,18 @@ const mongoose = require('mongoose');
 
 
 const connectDB = () => {
-    const uri = process.env.ATLAS_URI;
-    // const uri = process.env.LOCAL_URI;
+
+    // const uri = process.env.ATLAS_URI;
+    // const uri = process.env.LOCAL_URI; 
+    // const uri = {
+    //     LOCAL_URI: process.env.LOCAL_URI,
+    //     ATLAS_URI: process.env.ATLAS_URI
+    // }
+    // mongoose.connect(uri[process.env.DB_SELECT], { useNewUrlParser: true, useCreateIndex: true }
+    // );
+
+    const uri = process.env[process.env.DB_SELECT];
+
     mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
     );
 
